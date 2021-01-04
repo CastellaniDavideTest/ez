34ce720bd859b13fb23f2b8609955a812e9c59c4
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-01-04 11:10:02 +0100

    Created bin/ez.py

---
+++ bin/ez.py
@@ -0,0 +1,19 @@
+"""ez
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-1-4"
+
+class ez:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(ez.ez())
+	
+	def ez():
+		"""ez first funtion
+		"""
+		return "ez" #for the test (see test_ez.py)
+		
+if __name__ == "__main__":
+	ez()
